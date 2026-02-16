8093b8fa81721b090b82eb9c21170e54fd18402d
udaypatil <adc@email.com>
2026-02-16 09:23:12 +0530

    modifcation in backend server.js file with comment lines

--- backend/server.js
+++ backend/server.js
@@ -2,33 +2,33 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 
-const app = express();
+const app = express();      //creating an instamce of express application 
 app.use(cors());
 app.use(express.json());
 
-const DATA_FILE = "./blogs.json";
+const DATA_FILE = "./blogs.json"; //path to the json file where blog are strored 
 
 // helper functions
 const readBlogs = () => {
-  return JSON.parse(fs.readFileSync(DATA_FILE, "utf-8"));
+  return JSON.parse(fs.readFileSync(DATA_FILE, "utf-8"));//convering json  to js object 
 };
 
 const writeBlogs = (blogs) => {
-  fs.writeFileSync(DATA_FILE, JSON.stringify(blogs, null, 2));
+  fs.writeFileSync(DATA_FILE, JSON.stringify(blogs, null, 2));//converting js object to json and writing it to the file 
 };
 
 // ================= ROUTES =================
 
 // GET all blogs
 app.get("/blogs", (req, res) => {
-  res.json(readBlogs());
+  res.json(readBlogs());//sending the list of blogs as a json response
 });
 
 // GET blog by id
 app.get("/blogs/:id", (req, res) => {
   const blogs = readBlogs();
   const blog = blogs.find(b => b.id == req.params.id);
-  blog ? res.json(blog) : res.status(404).json({ message: "Not found" });
+  blog ? res.json(blog) : res.status(404).json({ message: "Not found" });//if blog is found send it as json response otherwise srevers with 404 status and the message "not found"
 });
 
 // POST blog
@@ -37,19 +37,20 @@ app.post("/blogs", (req, res) => {
   const newBlog = {
     id: Date.now(),
     ...req.body
-  };
-  blogs.push(newBlog);
-  writeBlogs(blogs);
+  };     
+  blogs.push(newBlog);//adding the new blog to the list of blogs
+  writeBlogs(blogs);// sving the updated list of blogs to the file 
   res.status(201).json(newBlog);
 });
 
 // PUT blog
+//Find blog index by ID If exists: Merge old + new data Save file  Else → 404 error
 app.put("/blogs/:id", (req, res) => {
   const blogs = readBlogs();
   const index = blogs.findIndex(b => b.id == req.params.id);
 
   if (index !== -1) {
-    blogs[index] = { ...blogs[index], ...req.body };
+    blogs[index] = { ...blogs[index], ...req.body };//
     writeBlogs(blogs);
     res.json(blogs[index]);
   } else {
@@ -58,6 +59,7 @@ app.put("/blogs/:id", (req, res) => {
 });
 
 // DELETE blog
+//Find blog by id if exists: remove it from the list save file else : 404 error 
 app.delete("/blogs/:id", (req, res) => {
   const blogs = readBlogs();
   const id = req.params.id;
@@ -76,4 +78,4 @@ app.delete("/blogs/:id", (req, res) => {
 
 app.listen(5000, () => {
   console.log(" Server running on http://localhost:5000");
-});
+});//starting the server on prot 5000 and logging a message to the console when the server is up and running 
